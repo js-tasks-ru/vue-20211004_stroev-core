a7e4c69c1adef75cda97fb08de982fddd2901a83
Dmitriy Stroev <stroev.core@gmail.com>
2021-10-11 16:22:03 +0300

    calculator app created

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,36 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      opL: 1,
+      opR: 1,
+      operation: 'sum',
+    };
+  },
+  computed: {
+    res() {
+      if (typeof this.opL == 'number' && typeof this.opR == 'number') {
+        switch (this.operation) {
+          case 'sum':
+            return this.opL + this.opR;
+          case 'subtract':
+            return this.opL - this.opR;
+          case 'multiply':
+            return this.opL * this.opR;
+          case 'divide':
+            return this.opR > 0 ? this.opL / this.opR : 'Dividing by zero!';
+          default:
+            return '-';
+        }
+      } else {
+        return '-';
+      }
+    },
+  },
+  methods: {
+    selectOperator(e) {
+      this.operation = e.target.value;
+    },
+  },
+}).mount('#app');
