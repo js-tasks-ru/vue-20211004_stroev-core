2fc968959038935f5190a4e989fdced8792d1f31
Dmitriy Stroev <stroev.core@gmail.com>
2021-10-11 20:40:17 +0300

    meetup titles app created

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,21 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      activeID: 1,
+      activeTitle: '',
+    };
+  },
+  watch: {
+    activeID: function (val) {
+      fetchMeetupById(val).then((item) => (this.activeTitle = item.title));
+    },
+  },
+  methods: {
+    changeActiveId(e) {
+      this.activeID = e.target.value;
+    },
+  },
+}).mount('#app');
