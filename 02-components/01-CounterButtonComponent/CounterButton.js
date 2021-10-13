f35a4547c45c61677c0031796b4489ba1ce589a6
Dmitriy Stroev <stroev.core@gmail.com>
2021-10-13 21:55:32 +0300

    CounterButtonComponent and MeetupDescription tasks resolved

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,29 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: {
+    'update:count': null,
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    incrCount() {
+      return this.count + 1;
+    },
+  },
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `
+    <button
+      type="button"
+      @click="$emit('update:count', incrCount())"
+    >{{count}}</button>
+    `,
 });
