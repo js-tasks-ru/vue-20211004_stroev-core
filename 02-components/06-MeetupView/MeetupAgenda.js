5eec6d32d9c8c387df47b7449b509bc9dd4f8031
Dmitriy Stroev <stroev.core@gmail.com>
2021-10-14 17:23:52 +0300

    MeetupView task resolved

---
+++ 02-components/06-MeetupView/MeetupAgenda.js
@@ -0,0 +1,29 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+
+export default defineComponent({
+  name: 'MeetupAgenda',
+
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+    },
+  },
+
+  data() {
+    return {
+      MeetupAgendaItem,
+    };
+  },
+
+  template: `
+    <ul class="agenda">
+      <li class="agenda__item" v-for="item in agenda" :key="item">
+        <MeetupAgendaItem :agenda-item="item" />        
+      </li>
+    </ul>`,
+});
